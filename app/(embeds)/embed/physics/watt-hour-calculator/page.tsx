088361d2d69247bbfebc9d0d7df6f256b5dc3548
heyumarkhan <itsumarkhan1@gmail.com>
2025-12-22 21:58:41 +0500

    Added Watt-hour Calculator

---
+++ app/(embeds)/embed/physics/watt-hour-calculator/page.tsx
@@ -0,0 +1,12 @@
+import WattHourCalculator from '../../../../_components/calculators/WattHourCalculator';
+
+export default function WattHourCalculatorEmbed() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-4">
+      <div className="max-w-2xl mx-auto">
+        <WattHourCalculator />
+      </div>
+    </div>
+  );
+}
+
