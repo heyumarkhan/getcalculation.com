07271f46eb9b5f1029c7294c3f82944af6e3cb9a
heyumarkhan <itsumarkhan1@gmail.com>
2026-01-20 13:30:58 +0500

    Add Transformer Sizing Calculator for Physics category

---
+++ app/(embeds)/embed/physics/transformer-sizing-calculator/page.tsx
@@ -0,0 +1,11 @@
+import TransformerSizingCalculator from '../../../../_components/calculators/TransformerSizingCalculator';
+
+export default function TransformerSizingCalculatorEmbed() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-4">
+      <div className="max-w-2xl mx-auto">
+        <TransformerSizingCalculator />
+      </div>
+    </div>
+  );
+}
